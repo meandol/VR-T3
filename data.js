63aaf17addb28156ba71aeb045436fd47bc8edcf
meandol <samdori48@gmail.com>
2024-10-16 11:19:32 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,216 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-3",
+      "name": "파노라마3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.36842698293115994,
+          "pitch": 0.0707323832576563,
+          "rotation": 0,
+          "target": "1-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.34923291507709564,
+          "pitch": -0.09206846297766802,
+          "title": "상가 전면",
+          "text": "OOO 상가의 전면 디자인<div>심플함, 노멀함을 강조한 파사드</div><div>주간 및 야간 어디서나 접근이 용이</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-5",
+      "name": "파노라마5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.17190769274888318,
+          "pitch": 0.18589411834514813,
+          "rotation": 0,
+          "target": "2-6"
+        },
+        {
+          "yaw": -1.5605765573586226,
+          "pitch": -0.03559949625354264,
+          "rotation": 0,
+          "target": "0-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.474866397265405,
+          "pitch": -0.0363286478397562,
+          "title": "옥외 접근 경로 A",
+          "text": "A 방향 접근로"
+        }
+      ]
+    },
+    {
+      "id": "2-6",
+      "name": "파노라마6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "yaw": 1.7919130675077444,
+        "pitch": 0.17552933279383964,
+        "fov": 1.5396019870895838
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.814346894956036,
+          "pitch": 0.14663371756777188,
+          "rotation": 0,
+          "target": "4-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.811648197262822,
+          "pitch": 0.016986739217543345,
+          "title": "카운터",
+          "text": "고객 응대 및 주문이 가능한 공간"
+        }
+      ]
+    },
+    {
+      "id": "3-7",
+      "name": "파노라마7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-8",
+      "name": "파노라마8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.5911679665348437,
+          "pitch": -0.02978639002512473,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.16125505213309843,
+          "pitch": 0.3028503202249446,
+          "title": "Title",
+          "text": "Text"
+        },
+        {
+          "yaw": 0.43816653725608035,
+          "pitch": -0.2688119671947895,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-9",
+      "name": "파노라마9",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 512,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
